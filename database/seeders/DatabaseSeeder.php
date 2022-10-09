644c08b6666e45ace45f68e112e06cd1598cc3df
Daniel Larson <danlarson1990@outlook.com.au>
2022-10-09 22:37:12 +1100

    Updated the databaseSeeder to include a user for myself that has a set password that is not default. This can be updated later to include Admin permissions as needed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,19 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->myUser();
+    }
+
+    private function myUser()
+    {
+        User::create(
+            [
+                'name' => 'Daniel Larson',
+                'email' => 'danlarson1990@outlook.com.au',
+                'email_verified_at' => now(),
+                'password' => Hash::make('AsDf!23Ll'), // password
+                'remember_token' => Str::random(10),
+            ]
+        );
     }
 }
